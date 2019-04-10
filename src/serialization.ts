13db610b00cf583bd67b0eec6b9c8430a42b2518
Gian-Luca Frei <gianlucafrei@gmail.com>
2019-04-10 13:54:06 +0200

    improved signature serialization

--- src/serialization.ts
+++ src/serialization.ts
@@ -72,28 +72,35 @@ export function deserializePublicKey(serializedKey:string):PublicKey{
 
 export function serializeSignature(signature:Signature):string{
 
-    let obj = {
-        r: hexStringToBuffer(signature.r),
-        s: hexStringToBuffer(signature.s),
-        j: signature.j
-    }
+    // check that s has the same size as r 
+    if(signature.s.length != signature.r.length)
+        throw new Error('signature s has not the same length as r');
 
-    var buffer : Buffer = msgpack.encode(obj);
-    return bufferToHexString(buffer);
+    if(signature.j > 4)
+        throw new Error('invalid singature j');
+
+    // For efficiency we just concat all signature parts
+    let hexstr = signature.s + signature.r;
+    hexstr += '0' + signature.j;
+
+    return hexstr;
 
 }
 
 export function deserializeSignature(serializedSignature:string):Signature{
 
-    var buffer : Buffer = hexStringToBuffer(serializedSignature);
-    var obj = msgpack.decode(buffer);
+    if(serializedSignature.length % 2 != 0)
+        throw new Error('invalid hex string')
 
-    var signature = {
-        r: bufferToHexString(obj.r),
-        s: bufferToHexString(obj.s),
-        j: obj.j
-    }
+    let n = serializedSignature.length;
 
-    return signature;
+    let shex = serializedSignature.substring(0,(n-2)/2);
+    let rhex = serializedSignature.substring((n-2)/2,(n-2));
+    let jhex = serializedSignature.substring((n-2), n);
 
+    return {
+        s: shex,
+        r: rhex,
+        j: parseInt(jhex, 16)
+    };
 }
